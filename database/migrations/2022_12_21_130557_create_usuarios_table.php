37d39b5f11c62d5228931610fced35f4acb2cf01
iñigo <inigocampo08@gmail.com>
2022-12-21 14:23:28 +0100

    migra

---
+++ database/migrations/2022_12_21_130557_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 30)->nullable();
+            $table->integer('edad');
+            $table->string('telefono', 35)->nullable();
+            $table->string('direccion', 75)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
