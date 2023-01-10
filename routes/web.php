cb2dab86a063f99aeed2ee7a6d1ab6392cffd367
cip <ciprianclase@gmail.com>
2023-01-10 10:56:17 +0100

    cosas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::view('/','login');
 
-Route::get('/', function () {
-    return view('login');
+Route::view('/register','register');
+
+Route::view('/login','login');
+
+Route::view('/header','header');
+
+Route::view('/ventanaPuntuacion','ventanaPuntuacion');
+
+Route::view('/ventanaPrueba','ventanaPrueba');
+
+Route::view('/ventanaGrupos','ventanaGrupos');
+
+Route::view('/ventanaEstadisticas','ventanaEstadisticas');
+
+// Route::view('/menuPrincipal','menuPrincipal');
+
+Route::get('/menuPrincipal', function(){
+    return 'menuPrincipal';
 });
+
+Route::view('/areaPersonal','areaPersonal');
