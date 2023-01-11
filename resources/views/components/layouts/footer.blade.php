164582651138d08d01cf123802cd65a0a9d686ab
ciprian196 <ciprianclase@gmail.com>
2023-01-12 00:06:18 +0100

    dc

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -19,14 +19,14 @@ a{
 
 </style>
 
-<footer>
-<div  class="container-fluid">
+<footer style="position: absolute">
+<div  class="container-fluid" >
     <div class="row" style="background-color:	#252525; height:40px;">
         <div class="col-xl-4 col-sm-3 col-6" style= height: 220px; >
             <p class="fs-6">©2023 por Venenum</p>
          </div>   
         <div class="col-xl-4 col-sm-3 col-6" style= height: 220px;>
-            <p class="fs-6 align-text-middle"><a href="href="mailto:ciprianclase@correo.com"">¿Tienes dudas? Pregúntanos</a><p>
+            <p class="fs-6 align-text-middle"><a href="href="mailto:ciprianclase@gmail.com"">¿Tienes dudas? Pregúntanos</a><p>
          </div>
          <div class="col-xl-4 col-sm-3 col-6" style= height: 220px;>
              <p class="fs-6"><a href="https://www.google.com/maps/dir//cifp+txurdinaga/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0xd4e4fac87437727:0x364f27e82def0130?sa=X&ved=2ahUKEwjk2qLliL38AhXiRvEDHanuD1UQ9Rd6BAhZEAU">Como llegar</a><p>  
