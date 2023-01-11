6ffde7b212cbb194449202e1c88bcf285da7c788
cip <ciprianclase@gmail.com>
2023-01-11 08:54:17 +0100

    fd

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,12 +7,15 @@
   <meta name="author" content="Vincent Garreau" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link rel="stylesheet" media="screen" href="css/style.css">
+  <link rel="stylesheet" href="ParticlesJS.css">
   <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+  
     <x-layouts.header />
-    <x-layouts.nav />
+  
+    <div id="paticles-js">
 
     <x-layouts.footer />
 
@@ -20,11 +23,13 @@
 <!-- count particles -->
 
 <!-- particles.js container -->
-<div id="particles-js"></div>
+</div>
 
 <!-- scripts -->
 <script src="../particles.js"></script>
 <script src="js/app.js"></script>
+<script src="../../js/particles.min.js"></script>
+<script src="../../js/particlesjs-config.json"> </script>
 
 <!-- stats.js -->
 <script src="js/lib/stats.js"></script>
--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -1,10 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid" style="background-color: gray; text-align: center; color:greenyellow" >
+ 
+  <div class="container">
+    <div class="row" style="align-items: center">
+        <div class="offset-md-2 col-md-4 col-sm-4 col-6 border text-center" style="background-color: gray ;height: 50px; " style><a  class="nav-link"  href="{{ route('areaPersonal')}}">Area Personal</a></div>
+        <div class="col-md-4 col-sm-4 col-6 border text-center " style="background-color: gray ;height: 50px; "><a class="nav-link" href="{{ route('ventanaPuntuacion')}}">Estadisticas</a></div>
+    </div>
+
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  {{-- <div class="container-fluid" style="background-color: gray; text-align: center; color:greenyellow" >
 
     
-      <div id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
+
             <a  class="nav-link"  href="{{ route('ventanaEstadisticas')}}">Estadisticas</a>
           </li>
           <li class="nav-item">
@@ -12,10 +25,6 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('ventanaGrupos')}}">Grupos</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    </div> --}}
 
   
