512f46078e1133434011ae74c6de100b61f0b123
iñigo <inigocampo08@gmail.com>
2023-01-11 08:38:08 +0100

    -

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <div  class="container-fluid">
     <div class="row" style="background-color:	black ">
-      <div class="col-md-2 col-sm-3 col-6  " style= height: 220px; > <img src="img/LogoFinal.png" alt="" style="height:120px"></div>
-      <div class="col-md-1 col-sm-3 col-6 offset-md-9 " style= height: 220px; >  <img src="img/avatar.png" alt="" style="height:90px; margin-top: 15px;"> </div>    
+      <div class="col-md-2 col-sm-3 col-6  " style= height: 220px; > <a href="{{ route('home')}}"><img src="img/LogoFinal.png" alt="" style="height:120px"></a></div>
+      <div class="col-md-1 col-sm-3 col-6 offset-md-9 " style= height: 220px; ><a href="{{ route('areaPersonal')}}"><img src="img/avatar.png" alt="" style="height:90px; margin-top: 15px;"></a></div>    
     </div>
     <div class="row" style="background-color:	black ">
     </div>
