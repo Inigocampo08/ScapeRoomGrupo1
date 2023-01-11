758e39963f1440aadb466f07ec31655163458f15
iñigo <inigocampo08@gmail.com>
2023-01-11 10:01:14 +0100

    fondo guapo

--- resources/views/areaPersonal.blade.php
+++ resources/views/areaPersonal.blade.php
@@ -6,30 +6,26 @@
   <meta name="description" content="particles.js is a lightweight JavaScript library for creating particles.">
   <meta name="author" content="Vincent Garreau" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <link rel="stylesheet" media="screen" href="css/style.css">
+  {{-- <link rel="stylesheet" media="screen" href="css/style.css"> --}}
   <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 <body>
+
+
     <x-layouts.header />
     <x-layouts.nav />
-
+    <x-layouts.fondo />
+  
     <x-layouts.footer />
+    
+ 
+    
+    
 
-
-<!-- count particles -->
-
-<!-- particles.js container -->
-<div id="particles-js"></div>
-
-<!-- scripts -->
-<script src="../particles.js"></script>
-<script src="js/app.js"></script>
-
-<!-- stats.js -->
-<script src="js/lib/stats.js"></script>
 <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> --}}
 
 </body>
 </html>
--- resources/views/ventanaPrueba.blade.php
+++ resources/views/ventanaPrueba.blade.php
@@ -5,8 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prueba</title>
+    <link rel="stylesheet" href="/css/fondo.css">
+
 </head>
 <body>
     <h1>Prueba</h1>
+
+    <div id="particles-js">
+    </div>
+
+    <script src="/js/particles.min.js"></script>
+    <script src="/js/particlesjs-config.json"></script>
 </body>
 </html>
