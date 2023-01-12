1ab426e802fad0eb7a5c563291a63e430b7444e5
Xabier <xabier.trabajo.69@gmail.com>
2023-01-12 11:33:17 +0100

    a

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -4,9 +4,9 @@
   <header>
 <div  class="container-fluid">
     <div class="row" style="background-color:	#252525 ">
-      <div class="col-md-2 col-sm-3 col-6" style= height: 220px; > <a href="{{ route('home')}}"><img src="img/LogoFinal.png" alt="" style="height:120px"></a></div>
+      <div class="col-md-2 col-sm-3 col-6" style= "height: 220px; "> <a href="{{ route('home')}}"><img src="img/LogoFinal.png" alt="" style="height:120px"></a></div>
       {{-- <div></div><i class="fa-solid fa-right-from-bracket"></i> --}}
-      <div class="col-md-1 col-sm-3 col-2 offset-md-9 " style= height: 220px; ><a href="{{ route('areaPersonal')}}"><img src="img/avatar.png" alt="" style="height:90px; margin-top: 15px;"></a></div>    
+      <div class="col-md-1 col-sm-3 col-2 offset-md-9 " style= "height: 220px;" ><a href="{{ route('areaPersonal')}}"><img src="img/avatar.png" alt="" style="height:90px; margin-top: 15px;"></a></div>    
     </div>
 </div>
   </header>
