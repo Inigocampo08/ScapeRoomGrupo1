e35374cec7241aeab6cf335de448cf9116873863
iñigo <inigocampo08@gmail.com>
2023-01-12 14:28:32 +0100

    Infor

--- resources/views/areaPersonal.blade.php
+++ resources/views/areaPersonal.blade.php
@@ -43,6 +43,9 @@
     a{
         color: white;
     }
+    h3{
+      color: #00ff00;
+    }
     
     </style>
     </head>
@@ -54,24 +57,33 @@
         <x-layouts.nav />
     
         <div id="particles-js">
-            {{-- <div class="container-fluid"> 
-                <div class="row" style="position: absolute">
-                    {{-- <div class="offset-xl-3 col-xl-6 col-10 offset-1 text-center "style="background-color:red; height: 500px; font-size:16px"> --}}
-                    {{-- <div class="col-xl-4 col-4 text-center"><img src="/img/LogoFinal.png" ></div>
-                    <div class="col-xl-8 col-8 text-center">
-                      <label>Nombre:</label>
-                      <input type="text" class="form-control" placeholder="Nombre" name="nombre" value="Nombre" required>
+            <div class="container-fluid" style="position: fixed; left:200px; top:210px; height:650px;width:1050px;"> 
+                <div class="row" style="height:550px;width:1050px;">
+                    <div class="col-12 col-sm-12 col-xl-5" style="height:200px;width:200px;">
+                        <img src="/img/avatar.png" style="height:150px;width:150px;"><br><br>
+                        <h1 style="color: #00ff00">Admin</h1>
+                    </div>
+                    <div class="col-12 col-sm-12 col-xl-7" style="width:800px; heigth:700px;">
+                      <h3>Nombre:</h3>
+                      <input type="text" class="form-control" placeholder="Nombre" name="nombre" value="Nombre" width="20px"  required>
                       <br>
-                      <label>Apellido:</label>
+                      <h3>Apellido:</h3>
                       <input type="text" class="form-control" placeholder="Apellido" name="Apellido" value="Apellido" required>
                       <br>
-                      <label>Email:</label>
+                      <h3>Email:</h3>
                       <input type="text" class="form-control" placeholder="Email" name="Email" value="Email" required>
                       <br>
-                      <label>Imagen:</label>
-                      <input type="text" class="form-control" placeholder="Imagen" name="Imagen" value="Imagen" required>
-                    </div> --}}
-                    {{-- </div> --}}
+                      <h3>Imagen:</h3>
+                      <input type="file" name="foto" placeholder="FOTO" id="btn-foto">
+                      <br><br><br>
+                      <button type="submit" class="btn" style="border: 3px solid #00ff0084; 
+                      border-radius: 6px;
+                      color: #00ff00; 
+                      background-color: #00ff0084; 
+                      padding: 6px;">Guardar</button>
+                    </div>
+                </div>
+            </div>
                 
                  <footer style="position: absolute">
                     <div  class="container-fluid" >
@@ -89,7 +101,7 @@
                         </div>
                     </div>
                     </footer>
-                </div>
+        </div>
     
     
         <script src="/js/particles.min.js"></script>
@@ -102,3 +114,19 @@
 
 </body>
 </html>
+
+{{-- 
+{{-- <div class="offset-xl-3 col-xl-6 col-10 offset-1 text-center "style="background-color:red; height: 500px; font-size:16px"> --}}
+                    {{-- <div class="col-xl-4 col-4 text-center"><img src="/img/LogoFinal.png" ></div>
+                    <div class="col-xl-8 col-8 text-center">
+                      <label>Nombre:</label>
+                      <input type="text" class="form-control" placeholder="Nombre" name="nombre" value="Nombre" required>
+                      <br>
+                      <label>Apellido:</label>
+                      <input type="text" class="form-control" placeholder="Apellido" name="Apellido" value="Apellido" required>
+                      <br>
+                      <label>Email:</label>
+                      <input type="text" class="form-control" placeholder="Email" name="Email" value="Email" required>
+                      <br>
+                      <label>Imagen:</label>
+                      <input type="text" class="form-control" placeholder="Imagen" name="Imagen" value="Imagen" required> --}}
