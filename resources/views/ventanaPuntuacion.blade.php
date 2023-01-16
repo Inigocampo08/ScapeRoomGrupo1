a6780a4b6444078422b31323545a75c507df34a3
iñigo <inigocampo08@gmail.com>
2023-01-16 10:43:29 +0100

    footer

--- resources/views/ventanaPuntuacion.blade.php
+++ resources/views/ventanaPuntuacion.blade.php
@@ -143,20 +143,20 @@
   <footer>
       <div class="container-fluid">
           <div class="row" style="background-color:#252525; height:119px;">
-              <div class="col-xl-4 col-sm-4 col-4">
-                <br>
+              <div class="col-xl-4 col-sm-4 col-4 text-center">
+                <br><br><br>
                   <p class="fs-6" style="color: 00ff00">©2023 por Venenum</p>
               </div>
-              <div class="col-xl-4 col-sm-4 col-4">
-                <br>
+              <div class="col-xl-4 col-sm-4 col-4 text-center">
+                <br><br><br>
                   <p class="fs-6"><a href="mailto:ciprianclase@gmail.com">¿Tienes dudas? Pregúntanos</a>
-                  <p>
+                  </p>
               </div>
-              <div class="col-xl-4 col-sm-4 col-4">
+              <div class="col-xl-4 col-sm-4 col-4 text-center">
                   <p class="fs-6">
-                    <br>
+                    <br><br><br>
                     <a href="https://www.google.com/maps/dir//cifp+txurdinaga/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0xd4e4fac87437727:0x364f27e82def0130?sa=X&ved=2ahUKEwjk2qLliL38AhXiRvEDHanuD1UQ9Rd6BAhZEAU">Como llegar</a>
-                  <p>
+                  </p>
               </div>
 
           </div>
