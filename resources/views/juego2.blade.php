dd21386e1660a1ab28ff30685aaca7f19c8f7b07
iñigo <inigocampo08@gmail.com>
2023-01-18 14:26:05 +0100

    juego2

--- resources/views/juego2.blade.php
+++ resources/views/juego2.blade.php
@@ -11,156 +11,61 @@ tent="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.
   {{-- <link rel="stylesheet" href="menuPrincipal.css"> --}}
   <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
-<style>
-        button {
-            background-color: transparent;
-            background-color: #00ff0084;
-            border: 3px solid #00ff0084;
-            border-radius: 6px;
-            color: #00ff00;
-            padding: 6px;
-        }
-        body{
-            background-color:#252525;
-        }
-
-        a :hover {
-            color: #89918984;
-        }
-
-        /* footer {
-            background-color: #252525;
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-            height: 50px;
-            color: white;
-            z-index: 2;
-             bottom:-10px;
-            display: flex;
-        } */
-
-        footer,a,h3, h1, p {
-            color: #00ff00;
-        }
-        ul {
-            list-style: none;
-        }
-
-        .campodetexto {
-
-            padding: 5px;
-            width: 75mvh;
-            border: 3px solid #00ff00;
-            border-radius: 6px;
-            background-color: #00ff0084;
-            margin-left: 10px;
-            margin-right: 10px;
-
-
-        }
-
-        .campodetexto:focus {
-
-            background-color: #00ff00;
-            border: 3px solid #00ff0084;
-
-        }
-
-        .boton:hover {
-
-            border: 3px solid #00ff00;
-            color: #033a0384;
-            background-color: #00ff00;
-
-        }
-
-        .boton {
-            border: 3px solid #00ff0084;
-            border-radius: 6px;
-            color: #00ff00;
-            background-color: #00ff0084;
-            padding: 6px;
-
-        }
-        hr{
-            color: #00ff00;
-        }
-    </style>
+            <link href="./css/bootstrap.min.css" rel="stylesheet">
+            <link href="./css/style.css" rel="stylesheet">
     </head>
 
     <body>
+        
 
         <x-layouts.header />
-
-            <div class="container-fluid" style="background-color:#252525;">
-                <div class="row">
-                <div class="col-10 col-sm-10 col-xl-10 offset-1" style="text-align: center;">
-                    <h1>Buscando la Formula</h1>
-                    <p> Necesitas completar el ejercicio para conseguir la formula para completar el antidoto. Para ello tienes que unir la Formula con su Nombre.
-                        Debes unir los 5 Formulas para poder entrar a la siguiente puerta. La salvacion esta en tus manos!!</p>
-                </div>
-                </div>
-                <div class="row" style="height: 490px;">
-                    <div class="col-6 col-sm-6 col-xl-6 text-center">
-                        <br><br>
-                        <h3><u>Nombre</u></h3>
+        <div id="todo">
+                <br><br>
+                <div id="explicacion" class="container">
+                    <div class="col-xl-10 col-sm-10 col-10 offset-1">
                         <br>
-                        <button name="button">Formaldehído</button><br><br>
-                        <button name="button">Galactosa</button><br><br>
-                        <button name="button">Ribosa</button><br><br>
-                        <button name="button">isohexano</button><br><br>
-                        <button name="button">3-metil-1-naftol</button>
-
-                    </div>
-                    <div class="col-6 col-sm-6 col-xl-6 text-center">
-                        <br><br>
-                        <h3><u>Formula</u></h3>
+                        <p style="color: black">Debes acertar la palabra oculta que se refieren a los Microorganismos estudiados en clase,
+                        las palabras son compuestas pero estan sin espacio. Debes ir eligiendo letra a letra hasta adivinar el Microorganismo.
+                        SOLO TIENES 8 INTENTOS </p>
                         <br>
-                        <button name="button">PCA</button><br><br>
-                        <button name="button">McConkey</button><br><br>
-                        <button name="button">CCA</button><br><br>
-                        <button name="button">KAA</button><br><br>
-                        <button name="button">VRBG</button><br>
-
                     </div>
                 </div>
-                <div class="row">
-                    <hr>
-                    <div class="col-4 col-sm-4 col-xl-4 text-center offset-4">
-                        <a href="{{ route('juego3')}}"><button name="button">Continuar</button></a><br>
-                    </div>
-                </div>
-
-            </div>
-
-            <footer>
-                <div class="container-fluid">
-                    <div class="row" style="background-color:#252525; height:119px;">
-                        <div class="col-xl-4 col-sm-4 col-4 text-center">
-                          <br><br><br>
-                            <p class="fs-6">©2023 por Venenum</p>
-                        </div>
-                        <div class="col-xl-4 col-sm-4 col-4 text-center">
-                          <br><br><br>
-                            <p class="fs-6"><a href="mailto:ciprianclase@gmail.com">¿Tienes dudas? Pregúntanos</a>
+                <br>
+                <main role="main" class="container" id="app">
+                    <div class="row">
+                        <div class="col-12">
+                            <h1 class="text-center">Juego de memorama</h1>
+                            <p>
+                                <span class="h5">Intentos: </span>
+                                {{intentos}}/{{MAXIMOS_INTENTOS}}&nbsp;<span class="h5">Aciertos:
+                                </span> {{aciertos}}
                             </p>
                         </div>
-                        <div class="col-xl-4 col-sm-4 col-4 text-center">
-                            <p class="fs-6">
-                              <br><br><br>
-                              <a href="https://www.google.com/maps/dir//cifp+txurdinaga/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0xd4e4fac87437727:0x364f27e82def0130?sa=X&ved=2ahUKEwjk2qLliL38AhXiRvEDHanuD1UQ9Rd6BAhZEAU">Como llegar</a>
-                            </p>
-                        </div>
-          
                     </div>
-                </div>
-            </footer>
+                    <div v-for="(fila, indiceFila) in memorama" :key="indiceFila"
+                        class="row">
+                        <div :key="indiceFila+''+indiceImagen" class="col-3"
+                            v-for="(imagen, indiceImagen) in fila">
+                            <div class="mb-4">
+                                <img @click="voltear(indiceFila, indiceImagen)"
+                                    :class="{'girar': imagen.mostrar}"
+                                    :src="(imagen.mostrar ? imagen.ruta :
+                                    NOMBRE_IMAGEN_OCULTA)" class="card-img-top img-fluid
+                                    img-thumbnail">
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            
+                <script src="./js/sweetalert2.all.min.js"></script>
+                <script src="./js/vue.min.js"></script>
+                <script src="./js/script.js"></script>
+        
+            <br><br>
+        </div>
+        <x-layouts.footer />
 
-    <script src="/js/particles.min.js"></script>
-    <script src="/js/particlesjs-config.json"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
