b1a96778e18cdc8b2320deead12bbd439338737a
parlitakeloke <trollert33@gmail.com>
2023-01-19 14:29:46 +0100

    Alberto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('apellidos', 30);
             $table->string('email')->unique();
             $table->string('password');
