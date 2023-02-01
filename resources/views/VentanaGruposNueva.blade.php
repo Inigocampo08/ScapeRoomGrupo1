1a2c74f48bede338b61bb41b54de35f4fb89df03
cip <ciprianclase@gmail.com>
2023-02-01 12:04:08 +0100

    df

--- resources/views/VentanaGruposNueva.blade.php
+++ resources/views/VentanaGruposNueva.blade.php
@@ -19,14 +19,7 @@
           color: #89918984;
       }
 
-      footer {
-          background-color: #252525;
-          bottom: 0px;
-          width: 99%;
-       
-          /* bottom:-10px; */
-        
-      }
+    
 
       ul {
           list-style: none;
@@ -76,9 +69,10 @@
           padding: 6px;
 
       }
-      #article {
+      #contendorPrincipal {
         
-    
+        min-height: calc(100vh -90px -80px);
+        background-color: #252525;
       }
      
     
@@ -90,30 +84,85 @@
   
     <main >
 
-    <div class="container-fluid" style="background-color: #252525;  height:83vh; ">
+    <div class="container-fluid" id="contendorPrincipal">
+        <br>
+        <br>
         <div class="row" >
-        
+            @auth
+            @if (Auth::user()->rol == "Profesor")
             @foreach ($grupos as $grupo )
-            <div class="col-xl-4 col-sm-4 col-4  text-center" style="border: 1px solid #00ff00 ; border-radius:22px;">
+               
+            <div class="col-xl-3 offset-sm-2 col-sm-3 offset-xl-2  offset-1 col-10  text-center" style="border: 2px solid #00ff00 ; border-radius:22px; heigth:70vh">
+                <br>
                 <h1 style="color: #00ff00">{{$grupo->Nombre}}</h1>
+                <br>
                 <b style="color: #00ff00">4 Usuarios</b>
+                <br>
+                <br>
             </div>
             <br>
             <br>
-
-            @endforeach
+            @endforeach 
+            @endif
+            @endauth
+          
+            @auth
+            @if (Auth::user()->rol == "Alumno")
+            @foreach ($grupos as $grupo )
+               
+            <div class="col-xl-3 offset-sm-2 col-sm-3 offset-xl-2  offset-1 col-10  text-center" style="border: 2px solid #00ff00 ; border-radius:22px; heigth:70vh">
+                <br>
+                <h1 style="color: #00ff00">{{$grupo->Nombre}}</h1>
+                <br>
+                <b style="color: #00ff00">4 Usuarios</b>
+                <br>
+                <br>
+            </div>
+            <br>
+            <br>
+            @endforeach 
+            @endif
+            @endauth
             
         </div>
-        <br>
-        <br>
-        <br>
+       <br>
         <div class="row">
             
             @auth
             @if(Auth::user()->rol == "Profesor")
-            <div class="offset-xl-4 col-xl-2 offset-sm-4  col-sm-2 offset-4  col-2 text-center" > <article id="article">
-                <button>AgregarGrupo</button>
+            <div class=" col-xl-12   col-sm-12   col-12 text-center" > <article id="article">
+                <button class="btn" data-bs-toggle="modal" data-bs-target="#botonEditar" style="background-color: #00ff00">
+                    AgregarGrupo
+                 </button>
+                <br>
             </div>
+            <div class="modal" id="botonEditar">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
+                        <div class="modal-header">
+                            <h5 class="modal-tittle">Editar usuario</h5>
+                            <button class="btn btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+                        <div class="modal-body">
+                     <form action="{{ route('user.update', Auth::user()->id) }}" method="POST" >
+                                @csrf
+                                {{ method_field('PUT')}}
+                                <label>Nombre:</label>
+                                <input type="text" class="form-control" name="nombre"  required>
+                                <br>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-success">ACTUALIZAR</button>
+                     </form>
+                            <button class="btn btn-success" data-bs-dismiss="modal">CANCELAR</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+               <br>
+               <br>
+               <br>
                
             @endif
             @endauth
