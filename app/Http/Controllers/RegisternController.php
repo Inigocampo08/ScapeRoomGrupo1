3c0effdd643069c164b09accb9b0141d01dcaddf
parlitakeloke <trollert33@gmail.com>
2023-02-02 10:53:27 +0100

    eqweqweqw

--- app/Http/Controllers/RegisternController.php
+++ app/Http/Controllers/RegisternController.php
@@ -19,7 +19,7 @@ class RegisternController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
         //
         if(Auth::user()->rol == "alumno"){
